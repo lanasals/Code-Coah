ea0249ddc917b66866bf4d5c305e7e517956aaf5
lanasals <126335149+lanasals@users.noreply.github.com>
2023-09-19 21:44:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
+// Quan el document estÃ  llest
 $(document).ready(function() {
+    
+    // Quan s'envia el formulari inclÃ²s a "codeForm" que inclou on lusuari posa el seu codi i on mopstrarem la resposta obtinguda de OpenAI
+	console.log(`in app.js`);
     $('#codeForm').submit(function(e) {
+        
+        // Prevenir l'acciÃ³ per defecte del formulari, No volem carregar una pagina nova
         e.preventDefault();
+        
+        // Esborrar el contingut del camp "newResultField" quan es fa clic al botÃ³ "Correct", per feedback pel usuari. que sapiga que pasa algo
+        $('#newResultField').val('');
+        
+        // Recollir el codi introduÃ¯t per lusuari al camp "codeInput"
         var code = $('#codeInput').val();
 
-        $.get('https://victorsala.me/CodeCoachAPI/response.php', { content: code }, function(data) {
+        // Realitzar la peticiÃ³ GET a l'API que cridarÃ¡ a OpenAI, estÃ  en servidor PHP per amagar lAPI_KEY  de lo contrari OpenAI deshabilita la key. No les vols pÃºbliques. Pasem 2 paerametres, el codi a examinar  (code)  i la opcio del desplegable (CoCoJob)
+        $.get('https://victorsala.me/CodeCoachAPI/response.php', { content: code, CoCoJob: CoCoJob }, function(data) {
+            
+            // Actualitzar el contingut del camp "newResultField" amb la resposta de l'API
             $('#newResultField').val(data.message);
+            
         }).fail(function() {
+            
+            // Mostrar un missatge d'error si la peticiÃ³ falla
             $('#newResultField').val('Error occurred during the request.');
         });
     });
 
-    const menu = document.querySelector('#mobile-menu');
-    const menuLinks = document.querySelector('.navbar__menu');
 
-    menu.addEventListener('click', function() {
-        menu.classList.toggle('is-active');
-        menuLinks.classList.toggle('active');
     });
-});
